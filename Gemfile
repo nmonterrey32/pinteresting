0fd15a7ec03a8a96cc15bdde4064e9c833b75458
Natalie Monterrey <nikkis_4@hotmail.com>
2016-07-25 16:36:23 -0700

    Implemented devise for users

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ group :doc do
 end
 
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
+  # Below is required for Heroku
+ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
